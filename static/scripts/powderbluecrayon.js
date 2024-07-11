25884005d1d86a3b7cd6bf43b137b4ad267deaa8
= <=>
2024-07-11 13:08:15 -0500

    change theme check on toggle. Add a function for loading theme from localStorage

--- static/scripts/powderbluecrayon.js
+++ static/scripts/powderbluecrayon.js
@@ -1,10 +1,20 @@
 function toggleDarkMode() {
 	document.documentElement.classList.toggle("dark")
 	const currentTheme = document.documentElement.getAttribute("data-theme")
-	if (currentTheme &&
+	if (!currentTheme ||
 		currentTheme === "corporate") {
 		document.documentElement.setAttribute("data-theme", "dim")
 	} else {
 		document.documentElement.setAttribute("data-theme", "corporate")
 	}
+	localStorage.setItem("theme", document.documentElement.getAttribute("data-theme"))
+}
+
+function loadTheme() {
+	const currentTheme = localStorage.getItem("theme") ?? "dim"
+	console.log(currentTheme)
+	document.documentElement.setAttribute(
+		"data-theme",
+		currentTheme
+	)
 }
