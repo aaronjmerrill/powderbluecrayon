396f688b55b6061ad6d8400988ad2925d4c4b682
Aaron M <ajm@powderbluecrayon.com>
2024-07-25 23:05:11 -0500

    Removed some lorem ipsum. Changed color of favicon. Added home button at top left. Added resume to Get my resume button

--- templates/root_templ.go
+++ templates/root_templ.go
@@ -28,7 +28,7 @@ func Root(children ...templ.Component) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\" class=\"font-serif\" data-theme=\"dim\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Powder Blue Crayon</title><!-- tailwind css --><link href=\"/static/css/tailwind.css\" rel=\"stylesheet\"><link href=\"/static/favicon.svg\" rel=\"icon\" type=\"image/x-icon\"></head><body class=\"p-5\" onload=\"loadTheme()\"><div id=\"top-nav\" class=\"justify-start flow-root space-x-1\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\" class=\"font-serif\" data-theme=\"dim\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Powder Blue Crayon</title><!-- tailwind css --><link href=\"/static/css/tailwind.css\" rel=\"stylesheet\"><link href=\"/static/favicon.svg\" rel=\"icon\" type=\"image/x-icon\"></head><body class=\"m-5\" onload=\"loadTheme()\"><div id=\"top-nav\" class=\"justify-start flow-root space-x-1\"><div class=\"btn btn-ghost\"><img src=\"static/favicon.svg\" alt=\"Logo\" class=\"float-start w-10 h-10\"> powder blue crayon</div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -46,7 +46,7 @@ func Root(children ...templ.Component) templ.Component {
 				return templ_7745c5c3_Err
 			}
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<script src=\"/static/scripts/htmx.min.js\"></script><script src=\"/static/scripts/powderbluecrayon.js\"></script><footer><div class=\"flex justify-center space-x-2\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<script src=\"/static/scripts/htmx.min.js\"></script><script src=\"/static/scripts/powderbluecrayon.js\"></script><footer class=\"pt-5\"><div class=\"flex justify-center space-x-2\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
